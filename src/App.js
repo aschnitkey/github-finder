bb04c0e312289da06512db392901458665b6c164
Andrew <aschnitkey@gmail.com>
2022-01-17 14:17:35 -0500

    alert context and reducer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
 
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
@@ -11,21 +12,23 @@ import Navbar from './components/layout/Navbar';
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className='flex flex-col justify-between h-screen'>
-          <Navbar />
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
 
-          <main className='container mx-auto px-3 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='*' element={<NotFound />} />
-            </Routes>
-          </main>
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='*' element={<NotFound />} />
+              </Routes>
+            </main>
 
-          <Footer />
-        </div>
-      </Router>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
